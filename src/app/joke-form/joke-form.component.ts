954891d157bc839d4a63264cb1e1d659d8c752e5
Chooi-Guan Lim <chooi-guan-lim@Chooi-Guans-MacBook-Pro.local>
2017-07-24 21:07:54 +0200

    CG angular 4 cli app

---
+++ src/app/joke-form/joke-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Joke } from '../joke/joke';
+
+@Component({
+  selector: 'app-joke-form',
+  templateUrl: './joke-form.component.html',
+  styleUrls: ['./joke-form.component.css']
+})
+export class JokeFormComponent implements OnInit {
+
+  @Output() jokeCreated = new EventEmitter<Joke>();
+
+  createJoke(setup: string, punchline: string) {
+    this.jokeCreated.emit(new Joke(setup, punchline));
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
